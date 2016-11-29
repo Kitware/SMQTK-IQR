Kitware/SMQTK-IQR

commit 5dd864c9e584df0598078d726640de32691095cc

Added optional use of file-magic module for DataFileElement
If importable, try to use file-magic mime-type identification from file
bytes first. This should be more performant that use of tika and is more
robust than just checking file name extension string.

setup.py
@@ -154,6 +154,9 @@ setuptools.setup(
         'flann': [
             'pyflann>=1.8.4',
         ],
+        'libmagic', [
+            'file-magic',
+        ],
         'postgres': [
             'psycopg2',
         ],

commit 62117198bba3387e1c7a40a5e96e37d18e9d3c2f

Fix syntax error in setup.py introduced by 5dd864c

setup.py
@@ -154,7 +154,7 @@ setuptools.setup(
         'flann': [
             'pyflann>=1.8.4',
         ],
-        'libmagic', [
+        'libmagic': [
             'file-magic',
         ],
         'postgres': [