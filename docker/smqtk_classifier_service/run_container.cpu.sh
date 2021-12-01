3ad697fffa3a79347444840eff98038cfaa834f7
Josh Anderson <josh.anderson@kitware.com>
2021-12-01 14:29:33 -0600

    Update Docker files and config files

---
+++ docker/smqtk_classifier_service/run_container.cpu.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Simple script for starting the SMQTK classifier service
+#
+
+# Container image to use
+CLASSIFIER_CONTAINER=kitware/smqtk/classifier_service
+CLASSIFIER_CONTAINER_VERSION="latest-cpu"
+CONTAINER_NAME="smqtk-classifier-service-cpu"
+CLASSIFIER_SERVICE_PORT=5002
+
+docker run \
+  -p ${CLASSIFIER_SERVICE_PORT}:5002 \
+  --name "${CONTAINER_NAME}" \
+  ${CLASSIFIER_CONTAINER}:${CLASSIFIER_CONTAINER_VERSION}
