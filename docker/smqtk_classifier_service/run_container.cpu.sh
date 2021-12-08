0cb6211353dcb288d0284baeb5a7019ec9d12c0a
Josh Anderson <josh.anderson@kitware.com>
2021-12-08 14:35:57 -0600

    Use env file variable for classifier container name

--- docker/smqtk_classifier_service/run_container.cpu.sh
+++ docker/smqtk_classifier_service/run_container.cpu.sh
@@ -4,7 +4,7 @@
 #
 
 # Container image to use
-CLASSIFIER_CONTAINER=kitware/smqtk/classifier_service
+CLASSIFIER_CONTAINER=${SMQTK_REGISTRY}/classifier_service
 CLASSIFIER_CONTAINER_VERSION="latest-cpu"
 CONTAINER_NAME="smqtk-classifier-service-cpu"
 CLASSIFIER_SERVICE_PORT=5002
